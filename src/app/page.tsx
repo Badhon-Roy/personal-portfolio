1bde79f362bf1bc35c3036da953b75860e68a40b
Badhon-Roy <roybadhon286@gmail.com>
2025-02-09 21:50:03 +0600

    basic project setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@
 const HomePage = () => {
   return (
     <div>
-      <h2 className='text-[#019fc7]'>This is home page</h2>
+      <h2 className='text-[#019fc7]'>This is demo home page</h2>
+      <button>Click me</button>
     </div>
   );
 };
