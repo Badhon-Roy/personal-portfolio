8fa00b70201c980a092d4640133e4e5dc78ceafe
Badhon-Roy <roybadhon286@gmail.com>
2025-02-09 21:37:51 +0600

    basic project setup done and navbar added

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectsPage = () => {
+    return (
+        <div>
+            <h2>This is project page</h2>
+        </div>
+    );
+};
+
+export default ProjectsPage;
---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,108 @@
+"use client"
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { IoMdCloseCircleOutline } from "react-icons/io";
+import { MdOutlineMenu } from "react-icons/md";
+
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleDrawer = () => {
+        setIsOpen(!isOpen);
+      };
+    
+      const handleNavClick = () => {
+        setIsOpen(false);
+      };
+
+  const navbarLinks = [
+    {
+      path: '/',
+      element: "Home"
+    },
+    {
+      path: '/about',
+      element: "About"
+    },
+    {
+      path: '/allProducts',
+      element: "Shop"
+    }
+  ]
+
+
+
+  return (
+    <>
+      {/* Desktop Navbar */}
+      <div className="sticky top-0 z-40 hidden bg-white shadow-md lg:block">
+        <div className="navbar max-w-[1400px] mx-auto flex items-center justify-between px-6">
+          {/* Logo */}
+          <Link href={"/"} className="flex items-center">
+            <Image
+              className="object-cover"
+              src="https://s3-eu-west-1.amazonaws.com/tpd/logos/615476e378a1a8001de58b7f/0x0.png"
+              alt="Logo"
+              width={90}
+              height={60}
+            />
+          </Link>
+
+          {/* Navigation Items */}
+          <div className="flex gap-8 font-bold">
+            {navbarLinks?.map((item, index) => (
+              <div
+                key={index}
+              >
+                {item?.element}
+              </div>
+            ))}
+          </div>
+
+        </div>
+      </div>
+
+      {/* Mobile Drawer */}
+      <div className="relative z-40 lg:hidden">
+        {/* Drawer Toggle Button */}
+        <button
+          onClick={toggleDrawer}
+          className="fixed z-50 p-3 bg-[#fb5770] text-white rounded-full top-4 right-4 shadow-md"
+        >
+          <MdOutlineMenu size={24} />
+        </button>
+
+        {/* Drawer */}
+        <div
+          className={`fixed top-0 right-0 h-full w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out ${isOpen ? "translate-x-0" : "translate-x-full"
+            }`}
+        >
+          {/* Close Button */}
+          <button
+            onClick={toggleDrawer}
+            className="absolute top-4 left-4 p-3 bg-[#fb5770] text-white rounded-full shadow-md"
+          >
+            <IoMdCloseCircleOutline size={24} />
+          </button>
+
+          {/* Navigation Links */}
+          <nav className="flex flex-col px-6 mt-20 space-y-6 font-bold">
+            {navbarLinks?.map(
+              (item, index) => (
+                <button
+                  key={index}
+                  onClick={handleNavClick}
+                >
+                  {item?.element}
+                </button>
+              )
+            )}
+          </nav>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
